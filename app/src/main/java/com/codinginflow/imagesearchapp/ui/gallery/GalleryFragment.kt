d7351247979377fa226b15ab4ebe44fbad1f5db2
Felipe Pelegrino <felipe.pelegrino99@gmail.com>
2022-06-07 12:11:12 -0300

    refactor: create functions to galleryFragment

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import com.codinginflow.imagesearchapp.R
 import com.codinginflow.imagesearchapp.data.UnsplashPhoto
@@ -23,41 +24,12 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery), UnsplashPhotoAdapte
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentGalleryBinding.bind(view)
-        adapter = UnsplashPhotoAdapter(this)
-        binding.apply {
-            galleryRv.setHasFixedSize(true)
-            galleryRv.itemAnimator = null
-            galleryRv.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
-                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() }
-            )
-
-            adapter.addLoadStateListener { loadState ->
-                galleryPbr.isVisible = loadState.source.refresh is LoadState.Loading
-                galleryRv.isVisible = loadState.source.refresh is LoadState.NotLoading
-                retryBtn.isVisible = loadState.source.refresh is LoadState.Error
-                errorTv.isVisible = loadState.source.refresh is LoadState.Error
-
-                //empty view
-                if (loadState.source.refresh is LoadState.NotLoading
-                    && loadState.append.endOfPaginationReached
-                    && adapter.itemCount < 1
-                ) {
-                    galleryRv.isVisible = false
-                    emptyTv.isVisible = true
-                } else {
-                    emptyTv.isVisible = false
-                }
-            }
-
-            retryBtn.setOnClickListener { adapter.retry() }
-        }
-
+        setAdapter()
         setObservers()
+        setButtons()
         setHasOptionsMenu(true)
     }
 
@@ -90,14 +62,51 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery), UnsplashPhotoAdapte
         findNavController().navigate(action)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     private fun setObservers() {
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setAdapter() {
+        adapter = UnsplashPhotoAdapter(this)
+        binding.apply {
+            galleryRv.setHasFixedSize(true)
+            galleryRv.itemAnimator = null
+            galleryRv.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() }
+            )
+            adapter.addLoadStateListener { loadState -> handleLoadState(loadState) }
+        }
+    }
+
+    private fun handleLoadState(loadState: CombinedLoadStates) {
+        binding.apply {
+            galleryPbr.isVisible = loadState.source.refresh is LoadState.Loading
+            galleryRv.isVisible = loadState.source.refresh is LoadState.NotLoading
+            retryBtn.isVisible = loadState.source.refresh is LoadState.Error
+            errorTv.isVisible = loadState.source.refresh is LoadState.Error
+
+            //empty view
+            if (loadState.source.refresh is LoadState.NotLoading
+                && loadState.append.endOfPaginationReached
+                && adapter.itemCount < 1
+            ) {
+                galleryRv.isVisible = false
+                emptyTv.isVisible = true
+            } else {
+                emptyTv.isVisible = false
+            }
+        }
+    }
+
+    private fun setButtons() {
+        binding.retryBtn.setOnClickListener { adapter.retry() }
     }
 }
